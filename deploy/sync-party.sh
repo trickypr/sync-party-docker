d50cbd54fa3519cdddf8b1c75a34d23d3cff4074
trickypr <23250792+trickypr@users.noreply.github.com>
2023-11-25 18:16:26 +1100

    ✨ CLI

--- deploy/sync-party.sh
+++ deploy/sync-party.sh
@@ -4,3 +4,9 @@ mkdir -p /root/sync-party
 
 docker pull ghcr.io/trickypr/sync-party-docker:main
 docker run -d --restart unless-stopped -p 3000:3000 -p 4000:4000 -v /app/data:/root/sync-party:rw ghcr.io/trickypr/sync-party-docker:main
+
+# Setup the CLI
+chmod +x cli/*
+cp cli/sync-party /usr/local/bin/sync-party
+cp cli/sync-party-admins /usr/local/bin/sync-party-admins
+cp cli/sync-party-users /usr/local/bin/sync-party-users
